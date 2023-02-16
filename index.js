871ae873e795f81f60c494411c45093ac188c748
Amit Gupta <109641538+codewithAmitg@users.noreply.github.com>
2023-02-16 16:43:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swagDocument = YAML.load('./swag.yaml');
-const dbConnect = require('./config/dbConnect');
-const { errorHandler } = require('./middlewares/errorHandler');
-const app = express();
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 4000;
-const authRouter = require("./routes/authRoute");
-const cookieParser = require("cookie-parser");
-dbConnect();
-app.use('/api-docs1', swaggerUi.serve, swaggerUi.setup(swagDocument));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/api/user',authRouter);
-// app.use(notFound);
-app.use(errorHandler);
+// const dbConnect = require('./config/dbConnect');
+// const { errorHandler } = require('./middlewares/errorHandler');
+// const app = express();
+// const dotenv = require('dotenv').config();
+// const PORT = process.env.PORT || 4000;
+// const authRouter = require("./routes/authRoute");
+// const cookieParser = require("cookie-parser");
+// dbConnect();
+// app.use('/api-docs1', swaggerUi.serve, swaggerUi.setup(swagDocument));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use('/api/user',authRouter);
+// // app.use(notFound);
+// app.use(errorHandler);
 
 app.use('/',(req,res)=>{
     res.json({
