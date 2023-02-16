939e068689decc5301cb79fa5762df656f2bf9e9
Amit Gupta <109641538+codewithAmitg@users.noreply.github.com>
2023-02-16 17:25:52 +0545

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const swagDocument = YAML.load('./swag.yaml');
 // const { errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 // const dotenv = require('dotenv').config();
-// const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 // const authRouter = require("./routes/authRoute");
 // const cookieParser = require("cookie-parser");
 // dbConnect();
