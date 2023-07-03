536309a3053f26bcd902507278cc8364c95ac63c
codewithAmitg <codewithamitgupta@gmail.com>
2023-07-03 13:57:27 +0545

    adding @asscii code to the password section

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } =require("mongoose");
 const dbConnect = () =>{
     try{
         mongoose.set("strictQuery", false);
-        const conn=mongoose.connect(process.env.MONGODB_URL);
+        const conn= mongoose.connect(process.env.MONGODB_URL);
         console.log("Database Connected Successfully");
         console.log(process.env.MONGODB_URL);
 
